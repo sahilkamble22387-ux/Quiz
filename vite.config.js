bdf54bfb07d1c1df01287d151784e72e3d4fdeaa
Sahil Kamble <sahilkamble@Sahils-MacBook-Air.local>
2026-03-26 13:28:17 +0530

    chore: add Vercel config for Vite SPA deploy

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
         manualChunks: {
